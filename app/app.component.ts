dcb0f28ffb2407d8e6997aaf481bde6258843ced
KimCoding <jeokrang@hanmail.net>
2016-03-31 00:13:01 +0900

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by KimCoding on 2016. 3. 30..
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello, I am KimCoding.</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by KimCoding on 2016. 3. 30..
+ */
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
